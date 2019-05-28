a8f8cc5e22e95cf090d61c540e8822fe32bd0b6b
@darrennicol75 <darren.nicol75@gmail.com>
2019-05-28 22:09:08 +0100

    Second commit

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -4,7 +4,7 @@ require( 'pry' )
 require_relative('./models/pizza_order')
 also_reload('./models/*')
 
-# INDEX display all pizzas (GET request)
+# INDEX- display all pizzas (GET request)
 
 get '/pizza-orders' do
   @orders = PizzaOrder.all()
@@ -12,7 +12,7 @@ get '/pizza-orders' do
 end
 # The below determines what to do if pizza orders is typed into the browser bar.
 
-# NEW display a form to make a new pizza order
+# NEW- display a form to make a new pizza order
 # watch for naming convention, new wouldn't work after :id
 # so move before.
 
@@ -20,7 +20,7 @@ get '/pizza-orders/new' do
   erb(:new)
 end
 
-# CREATE create a database entry for new pizza order
+# CREATE- create a database entry for new pizza order
 post '/pizza-orders' do
   # binding.pry
   @order = PizzaOrder.new(params)
@@ -28,23 +28,30 @@ post '/pizza-orders' do
   erb(:create)
 end
 
+# EDIT- display a form to edit a pizza orders details
+get '/pizza-orders/:id/edit' do
+  @order = PizzaOrder.find( parama[:id] )
+  erb(:edit)
+end
+# UPDATE- updates a database entry for the edited pizza order
+post '/pizza-orders/:id/edit' do
+  order =PizzaOrder.new(params)
+  order.update()
+  redirect to '/pizza-orders'
+end
 
-# SHOW display one pizza
+# SHOW- display one pizza
 # /:id is a show command for id's
 get '/pizza-orders/:id' do
   @order =PizzaOrder.find(params[:id])
   erb(:show)
 end
-# you can use a binding.pry to check for errors.
+# remember you can use a binding.pry to check for errors.
 
-
-# EDIT display a form to edit a pizza orders details
-# UPDATE updates a database entry for the edited pizza order
-# DELETE deletes a pizza order from the database.
-delete '/pizza-orders/:id' do
-
-end 
-
-
-
-######
+# DELETE- deletes a pizza order from the database.
+post '/pizza-orders/:id/delete' do
+  order =PizzaOrder.find( params[:id])
+  order.delete()
+  erb(:delete)
+  # redirect to '/pizza-orders'
+end
